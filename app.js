139b1c89dbc11cdbde258aaa2e7a7b0bacf3ee8d
shahzodbekFrontend <shahzodbekrahmonjonov83@gmail.com>
2023-01-10 15:49:19 +0500

    card

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
-let prev = 0
-let calc = 5
-const sensitivity = 1
-container.addEventListener("mousedown", (e) => {
+let prev = 0;
+let calc = 5;
+const sensitivity = 1;
+container.addEventListener("mouseover", (e) => {
   const x = e.clientX;
-  container.addEventListener("mousedown", rotate);
+  container.addEventListener("mouseover", rotate);
   function rotate(e) {
     calc = (e.clientX + x) / sensitivity;
     card.style.transform = `
         rotateY(${calc + prev}deg)
-        rotateZ(${calc + prev}deg)
+        
         rotateX(${calc + prev}deg)
         `;
     body.style.cursor = "grabbing";
   }
   prev += calc;
-  window.addEventListener("mouseup", function () {
-    section.removeEventListener("mousemove", rotate);
-    body.style.cursor = "default";
-  });
+  // window.addEventListener("mouseover", function () {
+  //   section.removeEventListener("mouseout", rotate);
+  //   body.style.cursor = "default";
+  // });
 });
